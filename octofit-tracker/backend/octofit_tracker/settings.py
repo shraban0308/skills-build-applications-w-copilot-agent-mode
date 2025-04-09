838799281edf4d9a67baa027189f3c3a0e5959a6
shraban0308 <51346453+shraban0308@users.noreply.github.com>
2025-04-09 17:36:19 +0000

    Update views.py with API root and viewsets

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-%lej2a$2@&rfeyqtlgycv4y5zui9ih*--@+()!gcuuryeo1ayu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[REPLACE-THIS-WITH-YOUR-CODESPACE-NAME]-8000.app.github.dev']
 
 
 # Application definition
